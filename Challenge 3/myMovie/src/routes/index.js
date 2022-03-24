6ec9ed3b21f9e363204ee811c296044f6162ff31
johnvees <yohanesvelly23@gmail.com>
2022-03-24 19:35:54 +0700

    fixing route

--- Challenge 3/myMovie/src/routes/index.js
+++ Challenge 3/myMovie/src/routes/index.js
@@ -8,19 +8,7 @@ const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
-<<<<<<< HEAD
     <Stack.Navigator screenOptions={{headerShown: false}}>
-<<<<<<< HEAD
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="MovieDetail" component={MovieDetail} />
-=======
->>>>>>> ad842432c3ec5aecb39c8ae644238eb61e9eec08
-      <Stack.Screen name="Login" component={Login} />
-=======
-    <Stack.Navigator
-      screenOptions={{headerShown: false}}
-      initialRouteName={'Login'}>
->>>>>>> 77282afd6b4771c1d22e117f4a41ee7f80a7d593
       <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
