77282afd6b4771c1d22e117f4a41ee7f80a7d593
johnvees <yohanesvelly23@gmail.com>
2022-03-24 18:54:36 +0700

    after presentation

--- Challenge 3/myMovie/src/routes/index.js
+++ Challenge 3/myMovie/src/routes/index.js
@@ -1,17 +1,18 @@
-import {View, Text} from 'react-native';
 import React from 'react';
 
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, Login, MovieDetail, Register} from '../screens';
+import {Home, Login, Register} from '../screens';
 import {DetailsHeader} from '../components';
 
 const Stack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
-    <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Login" component={Login} />
+    <Stack.Navigator
+      screenOptions={{headerShown: false}}
+      initialRouteName={'Login'}>
       <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="Login" component={Login} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="DetailsHeader" component={DetailsHeader} />
     </Stack.Navigator>
