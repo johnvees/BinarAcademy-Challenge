76865760a6dc8a889919d9726c9d195040aad2f0
johnvees <yohanesvelly23@gmail.com>
2022-03-25 12:54:35 +0700

    change internet conenction checker to method 2 and fix route

--- Challenge 3/myMovie/src/routes/index.js
+++ Challenge 3/myMovie/src/routes/index.js
@@ -9,8 +9,8 @@ const Stack = createNativeStackNavigator();
 export default function Routes() {
   return (
     <Stack.Navigator screenOptions={{headerShown: false}}>
-      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Home" component={Home} />
       <Stack.Screen name="Register" component={Register} />
       <Stack.Screen name="DetailsHeader" component={DetailsHeader} />
     </Stack.Navigator>
